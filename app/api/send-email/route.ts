9a0c17b4cc93d29df2bf11b750e675c50c1fd092
Josjay05 <ojoshjay@gmail.com>
2024-06-11 16:48:23 +0100

    commit

--- app/api/send-email/route.ts
+++ app/api/send-email/route.ts
@@ -1,7 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import nodemailer from "nodemailer";
 
-
 const transporter = nodemailer.createTransport({
   host: "smtp.hostinger.com",
   port: 465,
@@ -13,47 +12,29 @@ const transporter = nodemailer.createTransport({
 });
 
 export async function POST(req: NextRequest) {
-  function isValidEmail(email: string): boolean {
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email.toLowerCase());
-  }
-  
   try {
     const { name, email, country, category, state } = await req.json();
 
-    
-
-    const emailResult = await transporter.sendMail({
+    await transporter.sendMail({
       from: '"Contact Form" <info@everywherewegoconsult.org>',
       to: "info@everywherewegoconsult.org",
       subject: "New Contact Form Submission",
-      text: `Name: ${name}\nEmail: ${email}\nCountry: ${country}\nCategory: ${category}\nState: ${state}`,
+      text: `
+        Name: ${name}
+        Email: ${email}
+        Country: ${country}
+        Category: ${category}
+        State: ${state}
+      `,
     });
 
-
-    if (!name || !email || !country || !category || !state) {
-      return new NextResponse(JSON.stringify({ message: "All fields are required" }), {
-        status: 400,
-        headers: { "Content-Type": "application/json" },
-      });
-    }
-
-    if (!isValidEmail(email)) {
-      return new NextResponse(JSON.stringify({ message: "Invalid email format" }), {
-        status: 400,
-        headers: { "Content-Type": "application/json" },
-      });
-    }
-
-    return new NextResponse(JSON.stringify({ message: "Email sent successfully" }), {
-      status: 200,
-      headers: { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" },
-    });
-  }  catch (error) {
+    return NextResponse.json({ message: "Email sent successfully" }, { status: 200 });
+  } catch (error) {
     console.error("Failed to send email:", error);
-    return new NextResponse(JSON.stringify({ message: "Failed to send email", error: error.message }), {
-      status: 500,
-      headers: { "Content-Type": "application/json" },
-    });
+    return NextResponse.json({ message: "Failed to send email", error }, { status: 500 });
   }
+}
+
+export function OPTIONS() {
+  return NextResponse.json({}, { headers: { "Access-Control-Allow-Origin": "*", "Access-Control-Allow-Methods": "GET,OPTIONS,PATCH,DELETE,POST,PUT", "Access-Control-Allow-Headers": "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" } });
 }

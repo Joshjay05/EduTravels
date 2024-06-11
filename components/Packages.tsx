9d32f6ced249dbc303d018e328d0aef10725afdc
Josjay05 <ojoshjay@gmail.com>
2024-06-11 12:02:42 +0100

    commit

--- components/Packages.tsx
+++ components/Packages.tsx
@@ -13,7 +13,7 @@ const Packages = () => {
 				adventure, relaxation, cultural experiences, or family fun, we have the
 				perfect package for you.
 			</p>
-			<div className="grid grid-cos-1 gap-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 py-12">
+			<div className="grid grid-cos-1 gap-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 py-12">
 				{PACKAGES.map((card) => (
 					<PackageItem
 						key={card.title}
@@ -35,45 +35,7 @@ interface PackagesItem {
 	des: string;
 	duration: string;
 }
-// const PackageItem = ({ title, URL, price, des, duration }: PackagesItem) => {
-// 	return (
-// 		<section className="overflow-hidden rounded-tr-xl border border-slate-200 group">
-// 			<article className="relative overflow-hidden">
-// 				<img
-// 					src={URL}
-// 					alt="img"
-// 					// height={510}
-// 					// width={510}
-// 					className=" group- hover:scale-105 group-hover:rotate-2 transition-all duration-500"
-// 				/>
-// 				<Link
-// 					href={"/"}
-// 					className="absolute top-1/2 left-1/2 h-14 w-14 bg-white flexCenter transition-x-1/2 -translate-y-1/2 scale-0 group-hover:scale-100 transition-all duration-500">
-// 					<RiSearchLine />
-// 				</Link>
-// 			</article>
-// 			<article className="p-4 bg-white">
-// 				<div className="capitalize medium-18 flexBetween">
-// 					{title} <span className="text-green-50 ">{price}</span>
-// 				</div>
-// 				<p className="text-gray-50 my-3 regular border-b border-gray-200 pb-3">
-// 					{des}
-// 				</p>
-// 				<div className="flexBetween">
-// 					<p className="flexStart  gap-2 text-gray-50">
-// 						<RiTimeLine />
-// 						<span className="medium">{duration}</span>
-// 					</p>
-// 					<Link
-// 						href="/"
-// 						className="medium-14 px-4 py-2 rounded-md border bg-black text-white">
-// 						<span> Book Now</span>
-// 					</Link>
-// 				</div>
-// 			</article>
-// 		</section>
-// 	);
-// };
+
 const PackageItem = ({ title, URL, price, des, duration }: PackagesItem) => {
 	return (
 		<section
